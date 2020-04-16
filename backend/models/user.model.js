27c6ac562c82d9235fed983bde896d58c5579b5b
Jose De la Cruz <jdelacruzjose@gmail.com>
2020-04-15 21:39:52 -0400

    User Model Complete

---
+++ backend/models/user.model.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const Schema   = mongoose.Schema;
+
+const userSchema = new Schema({
+    username: {type: String, unique: true, minlength: 3, required: true},
+    email: {type: String, unique: true, required: true},
+    password: {type: String, required: true},
+    firstName: String,
+    lastName: String,
+    imgUrl: {type: String},
+    age: Number,
+    gender: String,
+    followers: [{ type: Schema.Types.ObjectId, ref:'User'}],
+    },
+    {
+        timestamps: true
+    }
+);
+
+const User = mongoose.model('User', userSchema);
+module.exports =User;
