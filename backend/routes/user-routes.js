2623c241a04f45c8c2672af35c763c52ac5db844
Jose De la Cruz <jdelacruzjose@gmail.com>
2020-04-20 21:06:08 -0400

    User CRUD complete

--- backend/routes/user-routes.js
+++ backend/routes/user-routes.js
@@ -35,4 +35,36 @@ router.route('/add').post((req, res) => {
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
+router.route('/:id').get((req, res) =>{
+    User.findById(req.params.id)
+    .then(user => res.json(user))
+    .catch(err => res.status(400).json('Error ' + err))
+});
+
+router.route('/:id').delete((req, res) =>{
+    User.findByIdAndDelete(req.params.id)
+    .then(() => res.json('User Deleted'))
+    .catch(err => res.status(400).json('Error '+ err));
+});
+
+router.route('/update/:id').post((req, res) =>{
+    User.findByIdAndUpdate(req.params.id)
+    .then(user =>{
+        user.username = req.body.username;
+        user.email = req.body.email;
+        user.password = req.body.password;
+        user.firstName = req.body.firstName;
+        user.lastName = req.body.lastName;
+        user.imgUrl = req.body.imgUrl;
+        user.age = req.body.age;
+        user.gender = req.body.gender;
+        user.followers = req.body.followers;
+
+        user.save()
+        .then(()=> res.json('User updated'))
+        .catch(err => res.status(400).json('Error ' + err));
+    })
+    .catch(err => res.status(400).json('Error ' + err));
+});
+
 module.exports = router;
