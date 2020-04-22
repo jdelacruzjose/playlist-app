30496bb7df7be26c5fc88152685ffa5f74bed7ce
Jose De la Cruz <jdelacruzjose@gmail.com>
2020-04-21 22:02:13 -0400

    Navbar Setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Navbar from './components/navbar.component';
+// import PlaylistList from './components/playlist-list.component';
+// import EditPlaylist from './components/edit-playlist.component';
+// import CreatePlaylist from './components/create-playlist.component';
+// import CreateUser from './components/create-user.component';
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>hello world</h1>
-    </div>
+    <Router>
+      <Navbar />
+      <br/>
+      {/* <Route path= '/' exact component ={PlaylistList} />
+      <Route path= '/edit/:id' component ={EditPlaylist} />
+      <Route path= '/create' component ={CreatePlaylist} />
+      <Route path= '/user' component ={CreateUser} /> */}
+    </Router> 
   );
 }
 
